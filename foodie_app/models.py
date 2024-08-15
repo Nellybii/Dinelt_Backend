bf697afb1dea8edf84581152a323c840ca8f6cb5
Nellybii <nellychepkem2015@gmail.com>
2024-08-15 10:07:29 +0300

    Update models to remove default images and allow null values

--- foodie_app/models.py
+++ foodie_app/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
-from PIL import Image
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.files import File
-from datetime import timedelta
 import os
 
 class User(AbstractUser):
@@ -17,7 +15,7 @@ class User(AbstractUser):
     city = models.CharField(max_length=100)
     address = models.CharField(max_length=255)
     postal_code = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='profile_pics')
+    image = models.ImageField(upload_to='profile_pics', blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -38,52 +36,30 @@ class User(AbstractUser):
     )
 
     def save(self, *args, **kwargs):
-        if not self.image or not hasattr(self.image, 'path'):  # If no image is provided
-            default_image_path = os.path.join(settings.MEDIA_ROOT, 'profile_pics/default.jpg')
-            if os.path.exists(default_image_path):
-                with open(default_image_path, 'rb') as f:
-                    self.image.save('default.jpg', File(f), save=False)
         super(User, self).save(*args, **kwargs)
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField(max_length=1000)
-    image = models.ImageField(upload_to="post_images", default="default.jpg")
+    image = models.ImageField(upload_to="post_images", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Story(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stories')
     content = models.TextField(max_length=1000)
-    image = models.ImageField(upload_to="story_images", default="default.jpg")
+    image = models.ImageField(upload_to="story_images", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    expires_at = models.DateTimeField(null=True, blank=True)  
-# followers
-# reviews
-# Comments  
-# food category(3)
-# reservations category (3)
-# cart
-# bookings
-
-
-    def save(self, *args, **kwargs):
-        if not self.is_superuser:
-            if not self.image or not hasattr(self.image, 'path'):  
-                default_image_path = os.path.join(settings.MEDIA_ROOT, 'profile_pics/default.jpg')
-                if os.path.exists(default_image_path):
-                    with open(default_image_path, 'rb') as f:
-                        self.image.save('default.jpg', File(f), save=False)
-        super(User, self).save(*args, **kwargs)
+    expires_at = models.DateTimeField(null=True, blank=True)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=200)
     bio = models.CharField(max_length=300)
-    image = models.ImageField(upload_to="user_images", default="default.jpg")
+    image = models.ImageField(upload_to="user_images", blank=True, null=True)
     followers = models.IntegerField(default=0)
     following = models.IntegerField(default=0)
     posts = models.ManyToManyField('Post', related_name='Post', blank=True)
-    stories=models.ManyToManyField('Story', related_name='Story', blank=True)
+    stories = models.ManyToManyField('Story', related_name='Story', blank=True)
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=255)
@@ -91,7 +67,7 @@ class Restaurant(models.Model):
     city = models.CharField(max_length=100) 
     country = models.CharField(max_length=100)  
     phone_number = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='restaurant_images', default='default.jpg')
+    image = models.ImageField(upload_to='restaurant_images', blank=True, null=True)
     description = models.TextField(blank=True)
 
     def __str__(self):
@@ -102,7 +78,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images', default='default.jpg')
+    image = models.ImageField(upload_to='menu_images', blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -141,4 +117,3 @@ class Reservation(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.get_or_create(user=instance, username=instance.username)
-
